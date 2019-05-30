e5e0700aace1b369745023af17c0885b947488ea
mdrobelahmedcse <mdrobelahmedcse@gmail.com>
2019-05-30 07:57:38 -0600

    full blog system done with laravel framework

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Category;
+use App\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        $posts = Post::latest()->approved()->published()->take(6)->get();
+        return view('home_page',compact('categories','posts'));
+    }
+}
